87746462a821cb7f62341ba2f5977240573b2f27
chittaranjan <chittaranjan.swain@oracle.com>
2019-03-09 18:34:50 +0530

    Showing progress bar while dowloading file from OCI OS

--- DownloadExportedNosqlZipFromOCIObjectStore.py
+++ DownloadExportedNosqlZipFromOCIObjectStore.py
@@ -4,7 +4,15 @@
 import filecmp
 import oci
 from oci.object_storage.models import CreateBucketDetails
+from time import sleep
+import sys
+import pprint
 
+
+
+
+def progress_callback(bytes_uploaded):
+    print("{} additional bytes uploaded".format(bytes_uploaded))
 
 config = oci.config.from_file()
 compartment_id = config["tenancy"]
@@ -16,10 +24,21 @@ object_name = "fm1915dev4iot_exported_20190304074251.zip"
 
 
 # Retrieve the file, streaming it into another file in 1 MiB chunks
-print('Retrieving file from object storage')
+head_obj = object_storage.head_object(namespace, bucket_name, object_name)
+total_length = head_obj.headers["Content-Length"]
 get_obj = object_storage.get_object(namespace, bucket_name, object_name)
 with open('test.zip', 'wb') as f:
-    for chunk in get_obj.data.raw.stream(1024 * 1024, decode_content=False):
-        f.write(chunk)
+    if total_length is None:  # no content length header
+        f.write(get_obj)
+    else:
+        dl = 0
+        total_length = int(total_length)
+        print('Downloading file form object storage')
+        for data in get_obj.data.raw.stream(1024 * 1024, decode_content=False):
+            dl += len(data)
+            f.write(data)
+            done = int(50 * dl / total_length)
+            sys.stdout.write("\r[%s%s] %s%%" % ('=' * done, ' ' * (50 - done), int((dl / total_length) * 100)))
+            sys.stdout.flush()
 
-print('Uploaded and downloaded files are the same: {}'.format(filecmp.cmp(object_name, 'test.zip')))
+#print('Uploaded and downloaded files are the same: {}'.format(filecmp.cmp(object_name, 'test.zip')))
